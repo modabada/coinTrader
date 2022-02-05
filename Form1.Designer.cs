fe84ae44323604bd98b4f44f7f0a1529e21c7509
modabada <miw1102@naver.com>
2022-02-05 18:54:53 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace coinTrader {
+    partial class Form1 {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btnClickThis = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClickThis
+            // 
+            this.btnClickThis.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnClickThis.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClickThis.Location = new System.Drawing.Point(340, 149);
+            this.btnClickThis.Name = "btnClickThis";
+            this.btnClickThis.Size = new System.Drawing.Size(75, 23);
+            this.btnClickThis.TabIndex = 0;
+            this.btnClickThis.Text = "ㅎㅇㅎㅇ";
+            this.btnClickThis.UseVisualStyleBackColor = false;
+            this.btnClickThis.Click += new System.EventHandler(this.btn1);
+            this.btnClickThis.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnClickThis_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(292, 225);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "lab";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClickThis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickThis;
+        private System.Windows.Forms.Label label1;
+        private void t() {
+            btnClickThis.Update();
+        }
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace coinTrader {
+    public partial class Form1: Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        private void btn1(object sender, EventArgs e) {
+            label1.Text = "test suc";
+            Console.WriteLine("true");
+            btnClickThis.Text = "test suc2";
+        }
+
+        private void btnClickThis_MouseClick(object sender, MouseEventArgs e) {
+
+        }
+    }
+}
