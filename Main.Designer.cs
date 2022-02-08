75fff2c1b701c747cd67810d2fc3c0ce23a1fc8e
modabada <miw1102@naver.com>
2022-02-08 14:33:05 +0900

    add func

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -18,15 +18,19 @@ namespace coinTrader {
         protected override bool ProcessCmdKey(ref Message msg, Keys keyData) {
             if(!base.ProcessCmdKey(ref msg, keyData)) {
                 if(keyData.Equals(Keys.Escape)) {
+                    this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
                     return true;
                 }
-                else {
-                    return false;
+                if(keyData.Equals(Keys.Tab)) {
+                    this.WindowState = System.Windows.Forms.FormWindowState.Normal;
+                    return true;
+                }
+                if(keyData.Equals(Keys.Enter)) {
+                    // start
+                    return true;
                 }
             }
-            else {
-                return true;
-            }
+            return false;
         }
 
         #region Windows Form 디자이너에서 생성한 코드
@@ -35,6 +39,7 @@ namespace coinTrader {
         private void InitializeComponent() {
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.background = new System.Windows.Forms.Panel();
+            this.Pause = new System.Windows.Forms.Button();
             this.Start = new System.Windows.Forms.Button();
             this.background.SuspendLayout();
             this.SuspendLayout();
@@ -43,6 +48,7 @@ namespace coinTrader {
             // 
             this.background.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
             this.background.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.background.Controls.Add(this.Pause);
             this.background.Controls.Add(this.Start);
             this.background.ForeColor = System.Drawing.Color.Black;
             this.background.Location = new System.Drawing.Point(0, 0);
@@ -52,20 +58,47 @@ namespace coinTrader {
             this.background.Size = new System.Drawing.Size(784, 462);
             this.background.TabIndex = 2;
             // 
+            // Pause
+            // 
+            this.Pause.BackColor = System.Drawing.Color.Black;
+            this.Pause.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Pause.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Pause.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.Pause.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Pause.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Pause.Font = new System.Drawing.Font("굴림", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Pause.ForeColor = System.Drawing.Color.DarkGray;
+            this.Pause.Location = new System.Drawing.Point(379, 309);
+            this.Pause.Margin = new System.Windows.Forms.Padding(0);
+            this.Pause.Name = "Pause";
+            this.Pause.Padding = new System.Windows.Forms.Padding(1);
+            this.Pause.Size = new System.Drawing.Size(120, 40);
+            this.Pause.TabIndex = 1;
+            this.Pause.TabStop = false;
+            this.Pause.Text = "Pause";
+            this.Pause.UseVisualStyleBackColor = false;
+            this.Pause.Click += new System.EventHandler(this.Pause_Click);
+            // 
             // Start
             // 
             this.Start.BackColor = System.Drawing.Color.Black;
+            this.Start.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.Start.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Start.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.Start.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.Start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Start.Font = new System.Drawing.Font("굴림", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.Start.ForeColor = System.Drawing.Color.DarkGray;
-            this.Start.Location = new System.Drawing.Point(347, 206);
+            this.Start.Location = new System.Drawing.Point(172, 309);
             this.Start.Margin = new System.Windows.Forms.Padding(0);
             this.Start.Name = "Start";
             this.Start.Padding = new System.Windows.Forms.Padding(1);
-            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.Size = new System.Drawing.Size(120, 40);
             this.Start.TabIndex = 0;
-            this.Start.Text = "button1";
+            this.Start.TabStop = false;
+            this.Start.Text = "Start";
             this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
             // 
             // Main
             // 
@@ -85,6 +118,7 @@ namespace coinTrader {
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.Panel background;
         private Button Start;
+        private Button Pause;
     }
 }
 
--- Main.cs
+++ Main.cs
@@ -6,19 +6,32 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace coinTrader {
+    public static class TradingInfo {
+        public static bool? state = false;
+        public static void Trading() {
+        }
+    }
     public partial class Main: Form {
         public Main() {
             FormBorderStyle = FormBorderStyle.Fixed3D;
             InitializeComponent();
+            Thread trader = new Thread(new ThreadStart(TradingInfo.Trading));
         }
-        private void btn1(object sender, EventArgs e) {
-            Console.WriteLine("true");
+        private void Start_Click(object sender, EventArgs e) {
+            Console.WriteLine("start");
+            Console.WriteLine(sender);
+            Console.WriteLine(e);
         }
 
         private void btnClickThis_MouseClick(object sender, MouseEventArgs e) {
+
+        }
+
+        private void Pause_Click(object sender, EventArgs e) {
 
         }
     }
