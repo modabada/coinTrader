c72c4840f8f8bd5356bcbd56308cd64fa2073c99
modabada <miw1102@naver.com>
2022-02-15 16:57:28 +0900

    key 로컬에서 로드, 키값 출력

--- Main.cs
+++ Main.cs
@@ -1,20 +1,20 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace coinTrader {
-    public static class TradingInfo {
-        public static bool? state = false;
-        public static void Trading() {
-        }
-    }
+    
     public partial class Main: Form {
         public Main() {
             FormBorderStyle = FormBorderStyle.Fixed3D;
@@ -25,14 +25,27 @@ namespace coinTrader {
             Console.WriteLine("start");
             Console.WriteLine(sender);
             Console.WriteLine(e);
-        }
+            JObject key = JObject.Parse(File.ReadAllText("/Users/inwoo/Desktop/Coding/coinTrader/Key.json"));
+            Console.WriteLine(key["Secret Key"]);
+            Console.WriteLine(key.GetValue("Connect Key"));
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create("https://api.bithumb.com/info/balance");
+            request.Method = "POST";
+            request.Headers.Add("apiKey", (string)key.GetValue("Connect Key"));
+            request.Headers.Add("secretKey", (string)key.GetValue("Secret Key"));
+            request.Headers.Add("currency", "ALL");
+            request.ContentType = "application/json";
 
-        private void btnClickThis_MouseClick(object sender, MouseEventArgs e) {
 
+            SampleOutput.Text = "";
         }
 
         private void Pause_Click(object sender, EventArgs e) {
 
+        }
+    }
+    public static class TradingInfo {
+        public static bool? state = false;
+        public static void Trading() {
         }
     }
 }
